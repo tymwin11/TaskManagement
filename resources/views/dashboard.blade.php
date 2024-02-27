a91077b67201263089181224878274ed93b67927
Timothy Nguyen <tim_nguyen11@yahoo.com>
2024-02-27 15:50:31 -0600

    add table title

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-4">
-                <div class="flex justify-end p-2">
+                <div class="flex justify-between p-2">
+                    <label class="text-xl font-semibold">
+                        Tasks
+                    </label>
                     <a href="{{ route('create-task') }}" type="button" class="btn btn-success text-black">
                         Create Task
                     </a>
